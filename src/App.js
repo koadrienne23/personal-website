4698bcde8c690272e34250a2a4c96b604b10d7d2
Adrienne Ko <koadrienne@Adriennes-MacBook-Pro.local>
2020-10-27 23:44:10 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router basename="/react-website">
+        <Router>
           <Header/>
           <ScrollIntoView>
             <Switch>
